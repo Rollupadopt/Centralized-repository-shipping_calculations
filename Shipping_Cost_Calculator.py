625cde59781d2c84764a34b8ddbdd84f0f361408
Boonyarit T <sky8152k@gmail.com>
2026-09-21 06:25:14 -0400

    Adding price after discount value

--- Shipping_Cost_Calculator.py
+++ Shipping_Cost_Calculator.py
@@ -11,3 +11,7 @@ shipping_cost = weight * rate
 print(f"Shipping Cost: {shipping_cost} USD")
 
 # Here is another update by Boonyarit T.
+discount = 0.1
+get_dis = shipping_cost - (shipping_cost * discount)
+remain_val = get_dis
+print(f'Price after discount = {remain_val}') 
